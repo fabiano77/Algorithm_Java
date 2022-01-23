d92ae60867cafbbb53fb38931db71614beb70adb
fabiano77 <fabiano7@naver.com>
2022-01-23 17:17:44 +0900

    update: BOJ #5582, 공통 부분 문자열 (풀이 변경)

--- src/baekjoon/Main_5582.java
+++ src/baekjoon/Main_5582.java
@@ -21,14 +21,30 @@ public class Main_5582 {
 		String s2 = br.readLine();
 		int cnt = 0;
 		int result = 0;
-		for(int i = 0; i+cnt <= s1.length(); i++) {
-			String target = s1.substring(i, i+cnt);
-			if(s2.contains(target)) {
-				result = cnt;
-				cnt++;
-				i--;
+//		for(int i = 0; i+cnt <= s1.length(); i++) {
+//			String target = s1.substring(i, i+cnt);
+//			if(s2.contains(target)) {
+//				result = cnt;
+//				cnt++;
+//				i--;
+//			}
+//		}
+		int [][] table = new int[s1.length()][s2.length()];
+		for(int j = 0; j < s2.length(); j++) {
+			if(s1.charAt(0)==s2.charAt(j)) table[0][j] = 1;			
+		}
+		for(int i = 0; i < s1.length(); i++) {
+			if(s1.charAt(i)==s2.charAt(0)) table[i][0] = 1;			
+		}
+		for(int i = 1; i < s1.length(); i++) {
+			for(int j = 1; j < s2.length(); j++) {
+				if(s1.charAt(i) == s2.charAt(j)) {
+					table[i][j] = table[i-1][j-1] + 1;
+					result = (result < table[i][j])? table[i][j]:result; 
+				}
 			}
 		}
+		
 		System.out.println(result);
 		
 	}
