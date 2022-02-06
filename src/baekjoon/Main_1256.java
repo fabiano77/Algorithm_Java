c923d189bb65efa203f7997f252867149625df03
김대희 <fabiano7@naver.com>
2022-02-07 01:37:09 +0900

    update: BOJ #1256, 사전

--- src/baekjoon/Main_1256.java
+++ src/baekjoon/Main_1256.java
@@ -3,7 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main_1256 {
@@ -13,7 +13,7 @@ public class Main_1256 {
 	static int K;
 	static int cnt;
 	static int[] answer;
-	static int[][] dp;
+	static BigInteger[][] dp;
 	static int cur;
 	static boolean isSolved;
 
@@ -25,67 +25,64 @@ public class Main_1256 {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		answer = new int[N+M];
-		dp = new int[N+M+1][N+M+1];
+		dp = new BigInteger[N+M+1][N+M+1];
 		
-		int combiValue = dpCombi(N+M, M);
-		if(combiValue < K) {
+		BigInteger combiValue = dpCombi(N+M, M);
+		if(combiValue.compareTo(new BigInteger(""+K)) < 0) {
 			System.out.println(-1);
 		}
 		else {
-			solve(K);
+			solve(new BigInteger(""+K));
 		}
 		
 
 	}
 	
 	// n개중 k를 순서없이 뽑는 경우의수
-	private static int dpCombi(int n, int k) {
+	private static BigInteger dpCombi(int n, int k) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n;j++) {
+				dp[i][j] = new BigInteger("0");
+			}
+		}
 		for(int i = 1; i <= k; i++) {
-			dp[i][i] = 1;
+			dp[i][i] = new BigInteger("1");
 		}
 		for(int i = 1; i <= n; i++) {
-			dp[i][0] = 1;
+			dp[i][0] = new BigInteger("1");
 		}
 		
 		for(int i = 2; i <= n; i++) {
 			for(int j = 1; j < i; j++) {
-				dp[i][j] = dp[i-1][j-1]+dp[i-1][j]; 
+				dp[i][j] = new BigInteger(dp[i-1][j-1].add(dp[i-1][j]).toString()); 
 			}
 		}
 		
 		return dp[n][k];
 	}
 	
-	private static void solve(int index) {
+	// index를 통해 경로 역추적
+	private static void solve(BigInteger index) {
 		int n = N+M;
 		int k = M;
-		while(true) {
-			// z를 선택
-			System.out.println("n = "+n+", k = "+k+", index = "+index);
-			if(index > dp[n-1][k-1]) {
-				index -= dp[n-1][k-1];
+		while(n != 0 || k != 0) {
+			// System.out.println("n = "+n +", k = "+k +", index = "+index);
+			
+			// k == 0 이거나 index <= dp[n-1][k] 이면 n-1 C k를 선택하고 a를 추가
+			if(k == 0 || index.compareTo(dp[n-1][k]) <= 0) {
+				sb.append('a');
 				n--;
-				sb.append('z');
 			}
-			// a를 선택
+			// index > dp[n-1][k] 이면 n-1 C k-1를 선택하고 z를 추가
 			else {
+				index = index.subtract(dp[n-1][k]);
+				sb.append('z');
 				n--;
 				k--;
-				sb.append('a');
-			}
-			if(dp[n][k] == 1) {
-				if(n == k) {
-					for(int i = 0; i < n; i++) {
-						sb.append('z');
-					}
-				}
-				else {
-					sb.append('a');
-				}
-				break;
 			}
 			
 		}
 		System.out.println(sb);
 	}
+	
 }
