ca797c769ff784648f43bc99a3d245653df735a6
김대희 <fabiano7@naver.com>
2022-02-13 23:50:44 +0900

    update: BOJ #11660, 구간 합 구하기 5

---
+++ src/baekjoon/Main_11660.java
@@ -0,0 +1,53 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11660 {
+	static int N, M;
+
+	public static void main(String[] args) throws IOException{
+		System.setIn(new FileInputStream("src/baekjoon/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N+1][N+1];
+		int[][] dp = new int[N+1][N+1];
+		
+		map = new int[N + 1][N + 1];
+		dp = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + map[i][j];
+			}
+		}
+			
+		
+		for(int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			System.out.println((dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]));
+			
+		}
+
+		
+		
+
+	}
+
+}
