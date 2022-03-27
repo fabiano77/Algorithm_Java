524006fedc7dec49956ed06a06b74aeedb8005e8
김대희 <fabiano7@naver.com>
2022-03-27 23:13:37 +0900

    update: BOJ #13146, 같은 수로 만들기 2

---
+++ src/baekjoon/Main_13146.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main_13146 {
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("src/baekjoon/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		
+		BigInteger cnt = new BigInteger("0");
+		int first = Integer.parseInt(br.readLine());
+		int prev = first;
+		int max = first;
+		for(int i = 1; i < N; i++) {
+			int next = Integer.parseInt(br.readLine());
+			max = Math.max(max, next);
+			
+			if(prev < next) {
+				cnt = cnt.add(new BigInteger(""+(next - prev)));
+			}
+			
+			prev = next;
+		}
+		if(prev < max) {
+			cnt = cnt.add(new BigInteger(""+(max - prev)));
+		}
+		System.out.println(cnt);
+		
+		
+
+	}
+
+}
