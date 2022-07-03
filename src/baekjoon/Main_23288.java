89731c2d4f632512da7a096f678ead5e9cca2f9c
김대희 <fabiano7@naver.com>
2022-07-03 21:12:00 +0900

    update: BOJ #10799, 쇠막대기

---
+++ src/baekjoon/Main_23288.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main_23288 {
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		int ans = 0;
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i)=='(') {
+				stack.push('(');
+			}
+			else {
+				stack.pop();
+				if(str.charAt(i-1)=='(') {
+					ans += stack.size();
+				}
+				else {
+					ans += 1;
+				}
+			}
+		}
+		System.out.println(ans);
+		br.close();
+	}
+
+}
